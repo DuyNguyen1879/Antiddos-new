f40a6b3ebd2ff84a3c7b6c1b26bfc9c499fcedb7
DuyNguyen1879 <51054574+DuyNguyen1879@users.noreply.github.com>
2020-02-23 15:14:02 +0700

    Update antiDDos.sh

--- antiDDos.sh
+++ antiDDos.sh
@@ -7,11 +7,7 @@ sudo systemctl disable firewalld
 sudo yum install -y iptables-services
 sudo systemctl enable iptables
 sudo systemctl start firewalld
-echo "Clonando repositorio..."
-sudo git clone https://github.com/abelantunes98/AntiDdos-CentOS
-cd AntiDdos-CentOS
 
-echo "Baixando dependencias."
 sudo yum install httpd-devel -y
 sudo yum install centos-release-scl -y
 sudo useradd httpserver -m -p $(openssl passwd -1 serverhttp12)
@@ -25,17 +21,19 @@ sudo rm apf-current.tar.gz
 cd apf-*
 sudo bash ./install.sh
 
-echo "Caso pergunte, selecione sobrescrever."
+
 cd ..
 rm -rf apf-*
-sudo mv conf.apf /etc/apf/conf.apf
+sed -i '/IFACE_UNTRUSTED/s/=".*"/="ens33"/' /etc/apf/conf.apf
+sed -i '/IG_TCP_CPORTS/s/=".*"/="22,80,443"/' /etc/apf/conf.apf
+sed -i '/IG_ICMP_TYPES/s/=".*"/="3,5,11,30"/' /etc/apf/conf.apf
+sed -i '/EG_TCP_CPORTS/s/=".*"/="21,80,443"/' /etc/apf/conf.apf
+echo BLK_MCATNET="0" >> /etc/apf/conf.apf
 sudo apf -s
 
 sudo wget https://codeload.github.com/shivaas/mod_evasive/zip/master
-sudo unzip master
+sudo unzip master -d /opt
 sudo yum install mod_evasive -y
-sudo mv mod_evasive-master /opt/
-sudo mv httpd.conf /opt/
 cd /opt/mod_evasive-master
 sudo apxs -i -c -a mod_evasive24.c
 systemctl restart httpd
@@ -43,7 +41,6 @@ systemctl restart httpd
 apachectl -M | grep evasive
 cd /etc/httpd/conf.d/
 cat > mod_evasive.conf << EOF
-LoadModule evasive20_module   /usr/lib64/httpd/modules/mod_evasive24.so
 <IfModule mod_evasive24.c>
                DOSHashTableSize    3097
                DOSPageCount        2
@@ -58,7 +55,7 @@ LoadModule evasive20_module   /usr/lib64/httpd/modules/mod_evasive24.so
 </IfModule>
 EOF
 
-at > ban_ip.sh << EOF
+cat > ban_ip.sh << \EOF
 #!/bin/sh
 # IP that will be blocked, as detected by mod_evasive
 IP=$1
@@ -71,6 +68,7 @@ $IPTABLES -I INPUT -s $IP -j DROP
 # Remove lock file for future checks
 rm -f "$MOD_EVASIVE_LOGDIR"/dos-"$IP"
 EOF
+
 sudo echo "apache ALL=NOPASSWD: /etc/httpd/conf.d/ban_ip.sh" >> /etc/sudoers
 sudo chmod  777 /etc/httpd/conf.d/ban_ip.sh
 sudo mkdir -p /var/log/mod_evasive
